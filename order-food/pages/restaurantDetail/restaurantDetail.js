22c6d65966de3e9ad8163a601f7eec93353f2ae3
TalentFly <1483118244@qq.com>
2018-07-07 01:00:36 +0800

    删除无用信息
    删除无用信息

--- order-food/pages/restaurantDetail/restaurantDetail.js
+++ order-food/pages/restaurantDetail/restaurantDetail.js
@@ -4,26 +4,6 @@ var api = require('../../config/api.js');
 Page({
   data: {
     bookToastHidden: true,
-    // navbar: [
-    //   {
-    //     machineName: '推荐',
-    //     image: 'https://machine-1256349011.cos.ap-beijing.myqcloud.com/mstj.jpg',
-    //     msg:'测试萨垛佛啊使得肌肤',
-    //     displaySequence: 0
-    //   },
-    //   {
-    //     machineName: '酒店概况',
-    //     image: 'https://machine-1256349011.cos.ap-beijing.myqcloud.com/restaurant.jpg',
-    //     msg: '测试萨垛佛啊使得肌肤',
-    //     displaySequence: 1
-    //   },
-    //   {
-    //     machineName: '饮食文化',
-    //     image: 'https://machine-1256349011.cos.ap-beijing.myqcloud.com/yswh.jpg',
-    //     msg: '测试萨垛佛啊使得肌肤',
-    //     displaySequence: 2
-    //   }
-    // ],
     currentTab: 0
   },
   navbarTap: function (e) {
